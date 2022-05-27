1b2fb891702b99ad10338b96c6d1df4265942e1b
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-05-27 09:26:05 +0500

    Final Commit Java File updated

--- app/src/main/java/com/example/mc_signin/MainActivity.java
+++ app/src/main/java/com/example/mc_signin/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.mc_signin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
+    private EditText username,pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText) findViewById(R.id.username);
+        pwd=(EditText) findViewById(R.id.pwd);
+        btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin")&&pwd.getText().toString().equals("123"))
+                {
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Wrong Credentials", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
